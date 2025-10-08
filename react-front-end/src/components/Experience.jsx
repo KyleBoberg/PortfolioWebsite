1f90804783e50b1c6bb3f3d9ea81993e6f10540f
Kyle Boberg <kwboberg2@gmail.com>
2025-10-07 21:11:43 -0400

    basic experience section layout

--- react-front-end/src/components/Experience.jsx
+++ react-front-end/src/components/Experience.jsx
@@ -10,7 +10,17 @@ export default function Experience() {
                     companyImage="https://media.licdn.com/dms/image/v2/D4E0BAQG3zol2k6sxCA/company-logo_100_100/company-logo_100_100/0/1693419702221/ts_partners_inc_logo?e=1762387200&v=beta&t=iIUDSbX15mdH8EtpKNphkd8DZ_gBwOUrAfHV3uKT55s" 
                     dateRange="July 2024 - Present"
                     title="Junior Software Developer" 
-                    bulletPoints={["bullet 1", "bullet 2"]}
+                    location="King of Prussia, PA"
+                    bulletPoints={[
+                        "Develop and maintain a comprehensive Java-based solution for corporate trust and stock transfer administration, trusted by many of the world's largest financial institutions.", 
+                        "Redesigned the system’s front-end with a focus on smart linking, activity suggestions, dynamic refreshing, and a clean and modern appearance.",
+                        "Lead the design and implementation of a user-friendly yet functionally robust data conversion tool used to convert newly acquired clients' legacy records into the system.",
+                        "Write and optimize complex SQL queries, often dramatically speeding up critical procedures.",
+                        "Participate in unit testing and code reviews to ensure the correctness of code and eliminate the possibility of bugs. "
+                    ]}
+                    skillTags={["Java", "SQL", "TypeScript", "HTML/CSS"]}
+                    color1={"#F29627"}
+                    color2={"rgba(184, 105, 8, 1)"}
                 />
 
                 <JobExperienceDiv
@@ -18,7 +28,17 @@ export default function Experience() {
                     companyImage="https://media.licdn.com/dms/image/v2/C4E0BAQGReviu2ICfiQ/company-logo_100_100/company-logo_100_100/0/1630604587825/amico_lane_logo?e=1762387200&v=beta&t=ZJEHfmXrreQkGoPcTZIuRz27yjlCcKvKRMqkoHD9QoU" 
                     dateRange="May 2023 - July 2024"
                     title="Software Development Intern/Consultant"
-                    bulletPoints={["bullet 1", "bullet 2"]}
+                    location="Washington, DC"
+                    bulletPoints={[
+                        "Asked to stay on at the real estate technology start-up following a summer internship to lead technical projects.", 
+                        "Led the technology development of a new company-wide KPI monitoring system. Built out multiple interactive KPI dashboards for use by both analysts and management.",
+                        "Developed and maintained automations for Amico Lane's cloud-based project management software to increase the speed of processing incoming client requests. ",
+                        "Integrated a variety of web services (incl. Google Workspace/Cloud and Xero Accounting) into the company's workflow using APIs and webhooks, allowing for reduced time for task completion and accurate KPI monitoring.",
+                        "Developed Amico Lane Condominium Target Visualizer, an interactive web-based internal tool to identify key target condominium associations to drive Amico Lane's expansion of its customer base. The system is rich with custom web-scraped data, displayed visually with the Google Maps SDK/API."
+                    ]}
+                    skillTags={["JavaScript", "ReactJS", "MongoDB", "Automations", "Google Cloud"]} 
+                    color1={"#FF4342"}
+                    color2={"#9e1010ff"}
                 />
             </div>
         </>
--- react-front-end/src/components/JobExperienceDiv.jsx
+++ react-front-end/src/components/JobExperienceDiv.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function JobExperienceDiv({company, companyImage, title, dateRange, bulletPoints}) {
+export default function JobExperienceDiv({company, companyImage, title, location, dateRange, bulletPoints, skillTags, color1, color2}) {
     
     const [isOpen, setIsOpen] = useState(false);
 
@@ -9,17 +9,82 @@ export default function JobExperienceDiv({company, companyImage, title, dateRang
     };
 
     return (
-        <div className="frosted-glass" >
+        <div className="frosted-glass" 
+            style={{
+                margin: "10px"
+            }}
+        >
+
             <div className="collapser"
                 onClick={toggleCollapse}>
-                <img src={companyImage} />
-                <h2>{company}</h2>
-                <h3>{title}</h3>
-                <h4>{dateRange}</h4>
+                <img 
+                    style={{borderRadius: '8px'}}
+                    src={companyImage} 
+                />
+                <div
+                    style={{
+                        width: "100%",
+                    }}
+                >
+                    {/* top part of header with company name, title, and date range */}
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "start",   
+                        }}>
+                        <div
+                            style={{flexGrow: 1}}
+                        >
+                            <h2>{company}</h2>
+                            <h3>{title}</h3>
+                        </div>
+                        <h4>{dateRange}</h4>
+                    </div>
+
+                    {/* bottom part of header with location and skill tags */}
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                        }}
+                    >
+                        <p
+                            style={{
+                                flexGrow: 1,
+                            }}
+                        >{location}</p>
+                        <div
+                            style={{
+                                display: "flex",
+                                gap: "5px",
+                                flexWrap: "wrap",
+                            }}>
+                            {skillTags && skillTags.map(tag => 
+                                <div className="skillTagDiv"
+                                    style={{
+                                        background: "linear-gradient(110deg, " + color1 + " 0%, " + color2 + " 100%)"
+                                    }}
+                                > 
+                                    <span className="skillTag">{tag}</span> 
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
 
-            <div className="collapsee" hidden={!isOpen}>
-                {bulletPoints.map(point => <p>{point}</p>)}
+            <div className={"collapsee" + (isOpen ? "" : " closed")} 
+                style={{
+                    marginLeft: "100px", //to skip space beneath image
+                    marginRight: "2rem",
+                }}
+            >
+                <ul>
+                {bulletPoints.map(point => <li>
+                    {point}
+                </li>)}
+                </ul>
             </div>
             
         </div>
